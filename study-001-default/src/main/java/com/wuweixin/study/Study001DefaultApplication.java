add2e230cfd3bd5a88aaff63d13bc6fb05cfa7bb
wuwx <wuweixin@gmail.com>
2021-05-05 22:11:02 +0800

    study-001-default

---
+++ study-001-default/src/main/java/com/wuweixin/study/Study001DefaultApplication.java
@@ -0,0 +1,13 @@
+package com.wuweixin.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study001DefaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study001DefaultApplication.class, args);
+    }
+
+}
