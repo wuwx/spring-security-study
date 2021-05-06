15571d23e2bfaddeb9f7db9cb403d68f1370a11b
wuwx <wuweixin@gmail.com>
2021-05-06 10:57:13 +0800

    增加 oauth2-client

---
+++ study-006-oauth2-client/src/main/java/com/wuweixin/study/Study006Oauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.wuweixin.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study006Oauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study006Oauth2ClientApplication.class, args);
+	}
+
+}
